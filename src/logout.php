84c8de2348a92fd3f1c415a81bc5e0330e4727f0
Godsbear <102647430+Godsbear@users.noreply.github.com>
2022-07-05 13:53:15 +0200

    Delete src directory

--- src/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //session starten für session-Variablen
-    session_start();
-    //session wird aufgelößt und Variablen gelöscht
-    session_destroy();
-    //weiterleitung zum login index.php
-    header('Location: ../index.php');
-    exit;
-?>
